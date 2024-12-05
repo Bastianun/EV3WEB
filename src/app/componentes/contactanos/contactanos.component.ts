40f13db98897e9e823b92b0786fc3560f97cfa7a
TronTeamM <matias.rojas.ibaceta@cftmail.cl>
2024-12-05 11:20:48 -0300

    primer comentario

--- src/app/componentes/contactanos/contactanos.component.ts
+++ src/app/componentes/contactanos/contactanos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { SolicitudFormularioService } from '../../servicios/solicitud-formulario/solicitud-formulario.service';
+import { TecnicosDeLaEmpresaService } from '../../servicios/tecnicos_de_la_empresa/tecnicos-de-la-empresa.service';
 
 @Component({
   selector: 'app-contactanos',
@@ -8,9 +9,10 @@ import { SolicitudFormularioService } from '../../servicios/solicitud-formulario
   styleUrl: './contactanos.component.css'
 })
 export class ContactanosComponent {
+  solicitudes_formulario:any;
   formularioForm;
   datos_formulario: any;
-  constructor(private formBuild: FormBuilder, private solicitudFormularioSrv:SolicitudFormularioService){
+  constructor(private formBuild: FormBuilder, private solicitudFormularioSrv:SolicitudFormularioService,private solicitudTecnicos:TecnicosDeLaEmpresaService ){
     this.formularioForm = this.formBuild.group({
       nombre: '',
       apellido: '',
@@ -20,7 +22,7 @@ export class ContactanosComponent {
     });
   }
   ngOnInit(){
-    
+    this.obtenerSolicitudesFormulario()
   }
   enviarDatos(){
     this.solicitudFormularioSrv.registrarFormulario(this.formularioForm.value).subscribe(
@@ -34,6 +36,18 @@ export class ContactanosComponent {
         console.log(error);
       }
     )   
+  }
+
+  obtenerSolicitudesFormulario(){
+    this.solicitudTecnicos.obtenerSolicitudesFormulario().subscribe(
+      (response:any) => {        
+        console.log(response)
+        this.solicitudes_formulario = response.profesionales;          
+        console.log(this.solicitudTecnicos);
+      }, error => {
+        console.log(error);
+      }
+    )
   }
 
 }
